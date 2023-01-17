4dc6b4fa314566aa0351cc9fc091287f37737591
Felipe MMS <fmagesty@gmail.com>
2023-01-17 09:12:51 -0300

    enh: make news to be fetched upon page render

--- pages/NewsScrapper.tsx
+++ pages/NewsScrapper.tsx
@@ -4,20 +4,24 @@ import fetchNews from "./api/fetchNews";
 import Link from "next/link";
 import Image from "next/image";
 import RenderNewsCards from "../components/RenderNewsCards";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 
 const NewsScrapper = () => {
-  const [cards, setCards] = useState(undefined)
+  const [cards, setCards] = useState(undefined);
 
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    let newsData = await fetchNews();
-    if (newsData) {
-      let cards = RenderNewsCards(newsData)
-      setCards(cards) // have to define the interface for this. tsx is sad
+  useEffect(() => {
+    const fetchData = async () => {
+      let newsData = await fetchNews();
+      if (newsData) {
+        let cards = RenderNewsCards(newsData);
+        setCards(cards); // have to define the interface for this. tsx is sad
+      }
+    };
+    fetchData()
+    return () => {
+      fetchData
     }
-  };
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -25,15 +29,8 @@ const NewsScrapper = () => {
         <title>Fetch News</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <button type={"submit"}>
-            <h1>Fetch Today&apos;s news from G1</h1>
-          </button>
-        </form>
-      </main>
-      <Link href="/">Go to Homepage</Link>
-      <>{cards && cards}</>
+        <Link href="/">Go to Homepage</Link>
+      {cards && cards}
     </div>
   );
 };
