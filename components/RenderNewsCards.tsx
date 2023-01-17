c9cdeab994f166a7fd600846ef008e975218a1d6
Felipe MMS <fmagesty@gmail.com>
2023-01-17 09:54:23 -0300

    style: add rendered news cards css

--- components/RenderNewsCards.tsx
+++ components/RenderNewsCards.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Image from "next/image";
-import Link from 'next/link'
+import Link from "next/link";
+import styles from "../styles/RenderNewsCards.module.css";
 
 const RenderNewsCards = (newsData: any) => {
   console.log("newsData:", newsData);
-  let cardList = []
+  let cardList = [];
   let headlineList = newsData.props.headlineList;
   let thumbnailList = newsData.props.thumbnailList;
   let newsLinkList = newsData.props.newsLinkList;
@@ -12,16 +13,22 @@ const RenderNewsCards = (newsData: any) => {
   for (let i = 0; i < headlineList.length; i++) {
     let title = headlineList[i];
     let thumb = thumbnailList[i];
-    let link = newsLinkList[i]
-    cardList.push((
-      <>
-        <h1 key={title}>{title}</h1>
-        <Image key={thumb} src={thumb} alt={thumb} width={200} height={200} />
-        <Link href={link}>Link</Link>
-      </>
-    ))
+    let link = newsLinkList[i];
+    cardList.push(
+      <div className={styles.cardOutterContainer}>
+        <Image key={thumb} src={thumb} alt={thumb} width={276} height={155} />
+        <div className={styles.cardInnerContainer}>
+          <h4 className={styles.cardHeadline} key={title}>
+            {title}
+          </h4>
+          <Link className={styles.cardLink} href={link}>
+            fonte
+          </Link>
+        </div>
+      </div>
+    );
   }
-  return cardList
+  return cardList;
 };
 
 export default RenderNewsCards;
