90b09c4a26c4523ecbcc953d32a0e7fcddcc3bd9
Felipe MMS <fmagesty@gmail.com>
2023-01-22 22:20:30 -0300

    fea: add more remotePatterns to nextconfig

--- next.config.js
+++ next.config.js
@@ -1,22 +1,43 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'imgs.xkcd.com',
+        protocol: "https",
+        hostname: "imgs.xkcd.com",
         // port: '',
         // pathname: '/account123/**',
       },
       {
-        protocol: 'https',
-        hostname: 's2.glbimg.com',
+        protocol: "https",
+        hostname: "s2.glbimg.com",
+        // port: '',
+        // pathname: '/account123/**',
+      },
+
+      // Double check which of these 3 are needed and if its just images:
+      {
+        protocol: "https",
+        hostname: "conteudo.imguol.com.br",
+        // port: '',
+        // pathname: '/account123/**',
+      },
+      {
+        protocol: "https",
+        hostname: "noticias.uol.com.br",
+        // port: '',
+        // pathname: '/account123/**',
+      },
+      {
+        protocol: "https",
+        hostname: "api.uol.com.br",
         // port: '',
         // pathname: '/account123/**',
       },
     ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
