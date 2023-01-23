fa858a1faf47085c4e3a9e5bc5bc8a650dbd14b6
Felipe MMS <fmagesty@gmail.com>
2023-01-22 22:20:11 -0300

    enh: optimize NewsScrapper's fetching method

--- pages/NewsScrapper.tsx
+++ pages/NewsScrapper.tsx
@@ -1,26 +1,34 @@
 import styles from "../styles/NewsScrapper.module.css";
 import Head from "next/head";
-import fetchNews from "./api/fetchNews";
+import fetchNewsG1 from "./api/fetchNewsG1";
+import fetchNewsUol from "./api/fetchNewsUol";
 import RenderNewsCards from "../components/RenderNewsCards";
 import { useState, useEffect } from "react";
 import Header from "../components/Header";
 import Loading from "../components/Loading";
 
 const NewsScrapper = () => {
-  const [cards, setCards] = useState(undefined);
+  const [cards, setCards] = useState<JSX.Element[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   useEffect(() => {
-    const fetchData = async () => {
-      let newsData = await fetchNews();
-      if (newsData) {
-        let cards = RenderNewsCards(newsData);
-        setCards(cards);
-      }
-    };
-    fetchData()
-    return () => {
-      fetchData
-    }
+    fetchNews("g1");
+    // fetchNews("uol");
   }, []);
+
+  const fetchNews = async (source: string) => {
+      let response;
+      setIsLoading(true);
+      if (source === "g1") {
+        response = await fetchNewsG1();
+      } else if (source === "uol") {
+        response = await fetchNewsUol();
+      }
+      let cards = RenderNewsCards(response);
+      setCards(cards);
+      setIsLoading(false);
+      return response;
+    };
 
   return (
     <div className={styles.mainContainer}>
@@ -28,8 +36,8 @@ const NewsScrapper = () => {
         <title>News Scrapper</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <Header/>
-      {cards ? cards && cards : <Loading/>}
+      <Header />
+      {isLoading ? <Loading /> : cards}
     </div>
   );
 };
