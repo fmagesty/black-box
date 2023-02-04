25f8de38166f25c2e21a00b1e631b14a133ec6d1
Felipe MMS <fmagesty@gmail.com>
2023-02-04 12:36:23 -0300

    chr: update tests to news cards and g1 fetcher

--- tests/fetchNewsG1.test.js
+++ tests/fetchNewsG1.test.js
@@ -1,19 +1,15 @@
-//fetchNewsG1 function test:
+import fetchNewsUol from "./fetchNewsUol";
+import RenderNewsCards from "./RenderNewsCards";
 
-// To test the fetchNewsG1 function, you can use Jest's mock function to mock the axios call and return a mocked response.
-// After that you can test the function if it returns a data object with the expected shape.
+describe("fetchNewsUol and RenderNewsCards", () => {
+  it("should fetch news data from the UOL website and render it as a list of cards", async () => {
+    const newsData = await fetchNewsUol();
+    expect(newsData).toHaveProperty("props");
+    expect(typeof newsData.props).toBe("object");
 
-it("should fetch data from G1 website", async () => {
-    const data = {
-      "props": {
-        "0": {
-            "headline": "Pneumonia causou 1/3 das mortes evitáveis de crianças ianomâmis de até 5 anos em 2022",
-            "href": "https://www1.folha.uol.com.br/equilibrioesaude/2023/01/pneumonia-causou-13-das-mortes-evitaveis-de-criancas-yanomamis-em-2022.shtml",
-        =-    "imageSrc": "https://conteudo.imguol.com.br/c/home/07/2023/01/22/criancas-yanomamis-sao-atendidas-por-agentes-de-saude-em-roraima-no-ultimo-dia-18-1674417774785_300x225.jpg"
-        },
-      }
-    };
-    axios.get = jest.fn().mockResolvedValue({ data });
-    const newsData = await fetchNewsG1();
-    expect(newsData).toEqual(data);
+    const cardList = RenderNewsCards(newsData);
+    expect(Array.isArray(cardList)).toBe(true);
+    expect(cardList.length).toBeGreaterThan(0);
+    expect(cardList[0].type).toBe("div");
   });
+});
