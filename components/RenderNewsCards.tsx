df3a4c02e8e8675e0d4001cdf519a0874c6d108b
Felipe MMS <fmagesty@gmail.com>
2023-02-06 12:14:06 -0300

    chr: refactor RenderNewsCards to tsx

--- components/RenderNewsCards.tsx
+++ components/RenderNewsCards.tsx
@@ -2,38 +2,44 @@ import React from "react"
 import Image from "next/image"
 import Link from "next/link"
 import styles from "../styles/RenderNewsCards.module.css"
+import { log } from "console"
+import { CLIENT_RENEG_LIMIT } from "tls"
 
-const RenderNewsCards = (newsData: any) => {
-    console.log("newsData:", newsData)
-    let cardList = []
-    for (let i = 0; i < Object.keys(newsData.props).length; i++) {
-        let headline = newsData.props[i].headline
-        let imageSrc = newsData.props[i].imageSrc
-        let href = newsData.props[i].href
-        let textSummary = newsData.props[i].textSummary
+interface News {
+    headline: string
+    imageSrc?: string
+    href: string
+    textSummary: string
+}
 
-        cardList.push(
-            <div className={styles.cardOutterContainer} key={i}>
-                {imageSrc ? (
-                    <Image
-                        key={imageSrc}
-                        src={imageSrc}
-                        alt={headline}
-                        width={400}
-                        height={300}
-                        aspect-ratio={"auto"}
-                        className={"imageSrc"}
-                    />
-                ) : null}
-                <div className={styles.cardInnerContainer}>
-                    <Link className={styles.cardLink} href={href}>
-                        <h2 className={styles.cardHeadline}>{headline}</h2>
-                    </Link>
-                    <p className={styles.newsSummary}>{textSummary}</p>
-                </div>
+interface NewsData {
+    props: News[]
+}
+
+const RenderNewsCards = (newsData: NewsData) => {
+    const newsArray = Object.values(newsData.props)
+    const cardList = newsArray.map((news, index) => (
+        <div className={styles.cardOutterContainer} key={index}>
+            {news.imageSrc ? (
+                <Image
+                    key={news.imageSrc}
+                    src={news.imageSrc}
+                    alt={news.headline}
+                    width={400}
+                    height={300}
+                    aspect-ratio={"auto"}
+                    className={"imageSrc"}
+                />
+            ) : null}
+            <div className={styles.cardInnerContainer}>
+                <Link className={styles.cardLink} href={news.href}>
+                    <h2 className={styles.cardHeadline}>{news.headline}</h2>
+                </Link>
+                <p className={styles.newsSummary}>{news.textSummary}</p>
             </div>
-        )
-    }
+        </div>
+    ))
+
     return cardList
 }
 
